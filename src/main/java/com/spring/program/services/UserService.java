064197c2624cde0f5b1cccafe9395a0378f1e164
pedro-githube <pedrobittetadeveloper@hotmail.com>
2022-02-24 00:11:08 -0300

    Insert User

--- src/main/java/com/spring/program/services/UserService.java
+++ src/main/java/com/spring/program/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.spring.program.domain.User;
+import com.spring.program.dto.UserDTO;
 import com.spring.program.repositories.UserRepository;
 import com.spring.program.services.exception.ObjectNotFoundException;
 
@@ -24,4 +25,12 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	} 
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
