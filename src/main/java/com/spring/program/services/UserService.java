9bd655142bf67d7f898bbdf55b4eeaa9439a05da
pedro-githube <pedrobittetadeveloper@hotmail.com>
2022-02-23 23:57:15 -0300

    FindById - Exception handling

--- src/main/java/com/spring/program/services/UserService.java
+++ src/main/java/com/spring/program/services/UserService.java
@@ -1,12 +1,14 @@
 package com.spring.program.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.spring.program.domain.User;
 import com.spring.program.repositories.UserRepository;
+import com.spring.program.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,9 @@ public class UserService {
 	public List<User> findAll(){
 	   return repository.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	} 
 }
