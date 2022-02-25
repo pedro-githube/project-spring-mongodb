a69479fa3ad8f58f9c48062a844e596e9fd16083
pedro-githube <pedrobittetadeveloper@hotmail.com>
2022-02-25 00:17:50 -0300

    FindById Post

---
+++ src/main/java/com/spring/program/services/PostService.java
@@ -0,0 +1,23 @@
+package com.spring.program.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.spring.program.domain.Post;
+import com.spring.program.repositories.PostRepository;
+import com.spring.program.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+}
