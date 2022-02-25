a834dff858826bab2884875b238cd85659334e05
pedro-githube <pedrobittetadeveloper@hotmail.com>
2022-02-25 01:05:31 -0300

    Add Comments

--- src/main/java/com/spring/program/config/Instantiation.java
+++ src/main/java/com/spring/program/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.spring.program.domain.Post;
 import com.spring.program.domain.User;
 import com.spring.program.dto.AuthorDTO;
+import com.spring.program.dto.CommentDTO;
 import com.spring.program.repositories.PostRepository;
 import com.spring.program.repositories.UserRepository;
 
@@ -38,14 +39,19 @@ public class Instantiation implements CommandLineRunner{
 		
 		userRepository.saveAll(Arrays.asList(maria,alex,bob));
 		
-		Post post1 = new Post(null, sdf.parse("24/02/2021"), "let's trip", "i'm going to travel to boston", new AuthorDTO(maria));
-		Post post2 = new Post(null, sdf.parse("10/05/2021"), "Good Morning", "i did wake up happy today", new AuthorDTO(maria));
+		Post post1 = new Post(null, sdf.parse("24/02/2022"), "let's trip", "i'm going to travel to boston", new AuthorDTO(maria));
+	    Post post2 = new Post(null, sdf.parse("10/05/2022"), "Good Morning", "i did wake up happy today", new AuthorDTO(maria));
 		
+	    CommentDTO c1 = new CommentDTO("Good trip !", sdf.parse("24/02/2022"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Enjoy the trip", sdf.parse("24/02/2012"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Have a Good day !", sdf.parse("10/05/2012"), new AuthorDTO(alex));
 		
-		postRepository.saveAll(Arrays.asList(post1,post2));
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
-		maria.getPosts().addAll(Arrays.asList(post1,post2));
+		postRepository.saveAll(Arrays.asList(post1, post2));
 		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
 		userRepository.save(maria);
 	}
 }
